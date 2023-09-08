4d1e4f48d8b806540f92d5f9321d1fff16c44196
Cory Sandlin <corybsa@gmail.com>
2023-09-08 09:58:01 -0400

    add comments

--- electron/ipc-events.ts
+++ electron/ipc-events.ts
@@ -1,6 +1,7 @@
 import { ipcMain, dialog } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as Protocol from './protocol';
 
 export class IpcEvents {
   mainWindow: Electron.BrowserWindow | null;
@@ -10,24 +11,32 @@ export class IpcEvents {
     this.setupHandleInterceptor();
   }
   
+  /**
+   * Initialized IPC handlers.
+   */
   init() {
+    // Open native file chooser dialog
     ipcMain.handle('open-file-chooser', async (event, args) => {
       return dialog.showOpenDialogSync(this.mainWindow!, { ...args, properties: ['openDirectory'] });
     });
     
+    // Format path separators to be correct on the current OS
     ipcMain.handle('format-separators', (event, args) => {
       const formattedPath = path.join(...args.split('/'));
       return formattedPath;
     });
 
+    // Get the path separator for the current OS
     ipcMain.handle('get-separator', (event, args) => {
       return path.sep;
     });
 
+    // Check if a file exists on the disk
     ipcMain.handle('check-file-exists', (event, args) => {
       return fs.existsSync(args);
     });
 
+    // Write a file to the disk
     ipcMain.handle('write-file', (event, args) => {
       try {
         fs.writeFileSync(args.path, args.payload);
@@ -37,6 +46,7 @@ export class IpcEvents {
       }
     });
 
+    // Create a directory (folder) on the disk
     ipcMain.handle('create-directory', (event, args) => {
       try {
         fs.mkdirSync(args, { recursive: true });
@@ -46,6 +56,7 @@ export class IpcEvents {
       }
     });
 
+    // Delete a file from the disk
     ipcMain.handle('delete-file', (event, args) => {
       try {
         fs.unlinkSync(args);
@@ -56,6 +67,10 @@ export class IpcEvents {
     });
   }
 
+  /**
+   * Intercepts calls to ipcMain.handle and checks if they came from a trusted source.
+   * If the source is trusted then the call is allowed to continue, otherwise it is blocked.
+   */
   private setupHandleInterceptor() {
     const originalHandle = ipcMain.handle;
     
@@ -70,8 +85,13 @@ export class IpcEvents {
     };
   }
 
+  /**
+   * Checks if the sender of a message is trusted.
+   * @param sender The origin of the message
+   * @returns true if the sender is trusted or false if it is not
+   */
   private isValidSender(sender: string): boolean {
-    if(sender === 'wg://rse') {
+    if(sender === `${Protocol.scheme}//rse`) {
       return true;
     }
 
