bf3c38a6f2c4e6acfd74e1e60ddecfd7b5c443be
Cory Sandlin <corybsa@gmail.com>
2023-09-08 09:58:32 -0400

    replace references to app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'custom-xp-curve'`, () => {
+  it(`should have as title 'secure-electron-angular-template'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('custom-xp-curve');
+    expect(app.title).toEqual('secure-electron-angular-template');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('custom-xp-curve app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('secure-electron-angular-template app is running!');
   });
 });
