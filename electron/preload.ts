0f8717fd5db7681bec0479f97963003f63ebdcb1
Cory Sandlin <corybsa@gmail.com>
2023-09-08 11:34:24 -0400

    create api for sending messages from main process to renderer process

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,21 +1,28 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
-export const ipcRequestMessages = {
+export const ipcRendererToMainMessages = {
   openFileChooser: 'open-file-chooser',
   formatSeparators: 'format-separators',
   getSeparator: 'get-separator',
   checkFileExists: 'check-file-exists',
   writeFile: 'write-file',
   createDirectory: 'create-directory',
-  deleteFile: 'delete-file'
+  deleteFile: 'delete-file',
 } as const;
 
-const validRequestMessages = Object.values(ipcRequestMessages);
-type IpcRequestMessage = typeof validRequestMessages[0];
+export const ipcMainToRendererMessages = {
+  toggleDarkMode: 'toggle-dark-mode',
+} as const;
+
+const validRendererToMainMessages = Object.values(ipcRendererToMainMessages);
+const validMainToRendererMessages = Object.values(ipcMainToRendererMessages);
+type IpcRendererToMainMessage = typeof validRendererToMainMessages[0];
+type IpcMainToRendererMessage = typeof validMainToRendererMessages[0];
 
 interface IWindowBridge {
   electronApi: {
-    invoke: (channel: IpcRequestMessage, data?: any) => Promise<any>;
+    invoke: (channel: IpcRendererToMainMessage, data?: any) => Promise<any>;
+    receive: (channel: IpcMainToRendererMessage, func: Function) => void;
   };
 }
 
@@ -25,13 +32,19 @@ export type IWindow = Window & typeof globalThis & IWindowBridge;
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
   'electronApi', {
-    invoke: (channel: IpcRequestMessage, data?: any) => {
+    invoke: (channel: IpcRendererToMainMessage, data?: any) => {
       // whitelist channels
-      if (validRequestMessages.includes(channel)) {
+      if (validRendererToMainMessages.includes(channel)) {
         return ipcRenderer.invoke(channel, data);
       }
 
       return new Promise(() => {});
+    },
+    receive: (channel: IpcMainToRendererMessage, func: Function) => {
+      if (validMainToRendererMessages.includes(channel)) {
+        // Deliberately strip `event` as it includes `sender` 
+        ipcRenderer.on(channel, (event, ...args) => func(...args));
+      }
     }
   }
 );
