ff16074c7b18bdbf144c16c80cb600d48806396d
Cory Sandlin <corybsa@gmail.com>
2023-09-08 09:58:38 -0400

    replace references to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'custom-xp-curve';
+  title = 'secure-electron-angular-template';
 }
